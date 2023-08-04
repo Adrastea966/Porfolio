9136e8286ac856f05f226bdc527e6dd9c8e7ad23
Adrastea <Adrastea966@yahoo.com>
2023-08-04 02:53:27 -0300

    Agregué nuevas animaciones y una nueva página

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+//Animacion de mascota
+function iniciarAnimacion() {
+    const gatoCuerpo = document.querySelector('.mascota');
+    gatoCuerpo.style.animation = 'avanzar 2s forwards';
+    gatoCuerpo.style.opacity = '1';
+
+    setTimeout(() => {
+        const ojos = document.querySelectorAll('.ojo-izquierdo, .ojo-derecho');
+        ojos.forEach(ojo => {
+            ojo.style.animation = 'mover-ojos 3s forwards';
+        });
+    }, 2000); 
+}
+
+setTimeout(iniciarAnimacion, 900);
